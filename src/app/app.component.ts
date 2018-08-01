22b89ebb361567b2a6e994fde015bb8b79e81655
dodevs <douglass.sousa@outlook.com.br>
2018-08-01 17:30:01 -0300

    Criado Array de Students para ser usado na diretiva do template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { Student } from './student/student.model';
 
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'jad';
+  // Criando objetos do tipo Student
+
+  // Declaração de Array com Genericos
+  students: Array<Student> = [
+    {name: 'Luke', isJedi: true, temple: 'Coruscant'},
+    {name: 'Leia', isJedi: false},
+    {name: 'Han Solo', isJedi: false}
+  ];
 }
